2d73b11dde0d2c032264e4143e979fafc961f4fc
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-12-20 11:31:45 +0530

    variable rules

---
+++ 2_rules_on_variable.php
@@ -0,0 +1,15 @@
+<h2>rule for creating variable in php</h2>
+<?php
+$rule1 = "starts with a $ sign";
+$rule2 = "cannot star with a number<br>";
+$rule3 = "must start with a letter or an underscore character<br>";
+$rule4 = "con only contain alphanumeric chcharacters and underscores<br>";
+$rule5 = "variable in php are case sensitive. ($ Satish. $ satish and $ sAtish are different).";
+
+echo "$rule1";
+echo "$rule2";
+echo "$rule3";
+echo "$rule4";
+echo "$rule5";
+
+?>
